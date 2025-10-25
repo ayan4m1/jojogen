ded6808f2211353878584650fb1673e4c8b83596
ayan4m1 <andrew@bulletlogic.com>
2025-10-24 23:21:47 -0400

    feat: get radar plot rendering on canvas

--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -1,4 +1,5 @@
 import { useFormik } from 'formik';
+import { contrastColor } from 'contrast-color';
 import { Button, Card, Form } from 'react-bootstrap';
 import { FastAverageColor } from 'fast-average-color';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,12 +7,16 @@ import { faDice, faRefresh } from '@fortawesome/free-solid-svg-icons';
 import { Fragment, useCallback, useEffect, useRef, useState } from 'react';
 
 import { StandForm } from '../types';
+import radarPlotBg from '../images/radar-plot-bg.png';
 import { BACKGROUND_COUNT, getPageTitle } from '../utils';
-import { contrastColor } from 'contrast-color';
+
+const radarRadiusCoefficient = 0.65;
+const RADIAN = Math.PI / 180;
 
 export function Component() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
+  // const [imageSrc, setImageSrc] = useState<string>(null);
   const [textColor, setTextColor] = useState<string>(null);
   const [backgroundIndex, setBackgroundIndex] = useState(1);
   const [backgroundSrc, setBackgroundSrc] = useState<string>(null);
@@ -26,13 +31,29 @@ export function Component() {
       precision: 3,
       potential: 3
     },
-    onSubmit: ({ name, master }) => {
+    onSubmit: ({
+      name,
+      master,
+      power,
+      speed,
+      range,
+      durability,
+      precision,
+      potential
+    }) => {
       if (!canvasRef.current || !containerRef.current) {
         return;
       }
 
       canvasRef.current.height = containerRef.current.offsetHeight;
       canvasRef.current.width = containerRef.current.offsetWidth;
+
+      // const image = new Image();
+
+      // image.onload = () => {
+      //   ctx.drawImage(image, width - 400, height - 100);
+      // };
+      // image.src = imageSrc;
 
       const ctx = canvasRef.current.getContext('2d');
       const { width, height } = canvasRef.current;
@@ -44,6 +65,60 @@ export function Component() {
       ctx.fillText(name, 40, 96);
       ctx.fillText('「 Stand Master 」', width - 400, height - 144);
       ctx.fillText(master, width - 400, height - 88);
+
+      const radarPlotBgImg = new Image();
+      const radarPlotCoords = [80, height - 300];
+
+      radarPlotBgImg.src = radarPlotBg;
+      radarPlotBgImg.onload = () => {
+        ctx.drawImage(
+          radarPlotBgImg,
+          radarPlotCoords[0],
+          radarPlotCoords[1],
+          256,
+          256
+        );
+      };
+
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = '#ff0000';
+
+      const maxRadius = (256 * radarRadiusCoefficient) / 2;
+      const stats = [durability, precision, potential, power, speed, range];
+
+      ctx.beginPath();
+
+      const radarPlotCenterCoords = [
+        radarPlotCoords[0] + 128,
+        radarPlotCoords[1] + 128
+      ];
+
+      for (let i = 5; i >= 0; i--) {
+        const startAngle = (i / 6) * 360;
+        const endAngle = ((i + 1) / 6) * 360;
+        const radius = maxRadius * (stats[i] / 6);
+        const nextRadius =
+          maxRadius * (stats[i + 1 === stats.length ? 0 : i + 1] / 6);
+
+        const x1 =
+          radarPlotCenterCoords[0] + Math.sin(-startAngle * RADIAN) * radius;
+        const y1 =
+          radarPlotCenterCoords[1] + Math.cos(-startAngle * RADIAN) * radius;
+        const x2 =
+          radarPlotCenterCoords[0] + Math.sin(-endAngle * RADIAN) * nextRadius;
+        const y2 =
+          radarPlotCenterCoords[1] + Math.cos(-endAngle * RADIAN) * nextRadius;
+
+        if (i === 0) {
+          ctx.moveTo(x1, y1);
+        }
+        ctx.lineTo(x2, y2);
+
+        console.dir(`${x1} ${y1} to ${x2} ${y2}`);
+      }
+
+      ctx.fillStyle = '#ff0000';
+      ctx.fill();
     }
   });
   const handleNewBackground = useCallback(
